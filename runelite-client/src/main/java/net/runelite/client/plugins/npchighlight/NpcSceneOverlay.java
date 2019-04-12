Str4tegik/runelite

npc indicators: add respawn timer

runelite-client/src/main/java/net/runelite/client/plugins/npchighlight/NpcSceneOverlay.java
@@ -30,23 +30,48 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
 import java.awt.Polygon;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.time.Instant;
+import java.util.Locale;
 import javax.inject.Inject;
 import net.runelite.api.Client;
 import net.runelite.api.NPC;
+import net.runelite.api.NPCComposition;
+import net.runelite.api.Perspective;
 import net.runelite.api.Point;
+import net.runelite.api.coords.LocalPoint;
+import net.runelite.api.coords.WorldPoint;
 import net.runelite.client.ui.overlay.Overlay;
 import net.runelite.client.ui.overlay.OverlayLayer;
 import net.runelite.client.ui.overlay.OverlayPosition;
 import net.runelite.client.ui.overlay.OverlayUtil;
 
-public class NpcClickboxOverlay extends Overlay
+public class NpcSceneOverlay extends Overlay
 {
+	// Anything but white text is quite hard to see since it is drawn on
+	// a dark background
+	private static final Color TEXT_COLOR = Color.WHITE;
+
+	/**
+	 * Estimated time of a game tick in seconds
+	 */
+	private static final double ESTIMATED_TICK_LENGTH = 0.6;
+
+	private static final NumberFormat TIME_LEFT_FORMATTER =
+		DecimalFormat.getInstance(Locale.US);
+
+	static
+	{
+		((DecimalFormat)TIME_LEFT_FORMATTER).applyPattern("#0.0");
+	}
+
 	private final Client client;
 	private final NpcIndicatorsConfig config;
 	private final NpcIndicatorsPlugin plugin;
 
 	@Inject
-	NpcClickboxOverlay(Client client, NpcIndicatorsConfig config, NpcIndicatorsPlugin plugin)
+	NpcSceneOverlay(Client client, NpcIndicatorsConfig config, NpcIndicatorsPlugin plugin)
 	{
 		this.client = client;
 		this.config = config;
@@ -58,6 +83,8 @@ public class NpcClickboxOverlay extends Overlay
 	@Override
 	public Dimension render(Graphics2D graphics)
 	{
+		renderNpcRespawns(graphics);
+
 		for (NPC npc : plugin.getHighlightedNpcs())
 		{
 			renderNpcOverlay(graphics, npc, npc.getName(), config.getHighlightColor());
@@ -66,22 +93,80 @@ public class NpcClickboxOverlay extends Overlay
 		return null;
 	}
 
+	private void renderNpcRespawns(Graphics2D graphics)
+	{
+		if (config.showRespawnTimer())
+		{
+			plugin.getDeadNpcsToDisplay().values().forEach(x ->
+			{
+				if (x.getPossibleRespawnLocations().isEmpty())
+				{
+					return;
+				}
+
+				WorldPoint respawnLocation = x.getPossibleRespawnLocations().get(0);
+				LocalPoint lp = LocalPoint.fromWorld(client,
+					respawnLocation.getX(), respawnLocation.getY());
+				if (lp != null)
+				{
+					Color color = config.getHighlightColor();
+
+					LocalPoint centerLp = new LocalPoint(
+						lp.getX() + Perspective.LOCAL_TILE_SIZE * (x.getNpcSize() - 1) / 2,
+						lp.getY() + Perspective.LOCAL_TILE_SIZE * (x.getNpcSize() - 1) / 2);
+					Polygon poly = Perspective.getCanvasTileAreaPoly(client, centerLp, x.getNpcSize());
+					if (poly != null)
+					{
+						OverlayUtil.renderPolygon(graphics, poly, color);
+					}
+
+					Instant now = Instant.now();
+					double timeLeft = ((x.getDiedOnTick() + x.getRespawnTime()) -
+						client.getTickCount()) * ESTIMATED_TICK_LENGTH -
+						(now.toEpochMilli() - plugin.getLastTickUpdate().toEpochMilli()) / 1000.0;
+					timeLeft = Math.max(0.0, timeLeft);
+					String timeLeftStr = TIME_LEFT_FORMATTER.format(timeLeft);
+					int textWidth = graphics.getFontMetrics().stringWidth(timeLeftStr);
+					int textHeight = graphics.getFontMetrics().getAscent();
+					Point canvasPoint = Perspective.worldToCanvas(client,
+						centerLp.getX(), centerLp.getY(), respawnLocation.getPlane());
+					if (canvasPoint != null)
+					{
+						Point canvasCenterPoint = new Point(
+							canvasPoint.getX() - textWidth / 2,
+							canvasPoint.getY() + textHeight / 2);
+						OverlayUtil.renderTextLocation(graphics, canvasCenterPoint, timeLeftStr, TEXT_COLOR);
+					}
+				}
+			});
+		}
+	}
+
 	private void renderNpcOverlay(Graphics2D graphics, NPC actor, String name, Color color)
 	{
 		switch (config.renderStyle())
 		{
 			case TILE:
-				Polygon objectTile = actor.getCanvasTilePoly();
+			{
+				int size = 1;
+				NPCComposition composition = actor.getTransformedComposition();
+				if (composition != null)
+				{
+					size = composition.getSize();
+				}
+				LocalPoint lp = actor.getLocalLocation();
+				Polygon tilePoly = Perspective.getCanvasTileAreaPoly(client, lp, size);
 
-				if (objectTile != null)
+				if (tilePoly != null)
 				{
 					graphics.setColor(color);
 					graphics.setStroke(new BasicStroke(2));
-					graphics.draw(objectTile);
+					graphics.draw(tilePoly);
 					graphics.setColor(new Color(color.getRed(), color.getGreen(), color.getBlue(), 20));
-					graphics.fill(objectTile);
+					graphics.fill(tilePoly);
 				}
 				break;
+			}
 
 			case HULL:
 				Polygon objectClickbox = actor.getConvexHull();

chrisjohnson/openosrs

runelite-client/src/main/java/net/runelite/client/plugins/npchighlight/NpcSceneOverlay.java
@@ -30,23 +30,45 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
 import java.awt.Polygon;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.time.Instant;
+import java.util.Locale;
 import javax.inject.Inject;
 import net.runelite.api.Client;
 import net.runelite.api.NPC;
+import net.runelite.api.NPCComposition;
+import net.runelite.api.Perspective;
 import net.runelite.api.Point;
+import net.runelite.api.coords.LocalPoint;
+import net.runelite.api.coords.WorldPoint;
 import net.runelite.client.ui.overlay.Overlay;
 import net.runelite.client.ui.overlay.OverlayLayer;
 import net.runelite.client.ui.overlay.OverlayPosition;
 import net.runelite.client.ui.overlay.OverlayUtil;
 
-public class NpcClickboxOverlay extends Overlay
+public class NpcSceneOverlay extends Overlay
 {
+	// Anything but white text is quite hard to see since it is drawn on
+	// a dark background
+	private static final Color TEXT_COLOR = Color.WHITE;
+
+	// Estimated time of a game tick in seconds
+	private static final double ESTIMATED_TICK_LENGTH = 0.6;
+
+	private static final NumberFormat TIME_LEFT_FORMATTER = DecimalFormat.getInstance(Locale.US);
+
+	static
+	{
+		((DecimalFormat)TIME_LEFT_FORMATTER).applyPattern("#0.0");
+	}
+
 	private final Client client;
 	private final NpcIndicatorsConfig config;
 	private final NpcIndicatorsPlugin plugin;
 
 	@Inject
-	NpcClickboxOverlay(Client client, NpcIndicatorsConfig config, NpcIndicatorsPlugin plugin)
+	NpcSceneOverlay(Client client, NpcIndicatorsConfig config, NpcIndicatorsPlugin plugin)
 	{
 		this.client = client;
 		this.config = config;
@@ -58,6 +80,11 @@ public class NpcClickboxOverlay extends Overlay
 	@Override
 	public Dimension render(Graphics2D graphics)
 	{
+		if (config.showRespawnTimer())
+		{
+			plugin.getDeadNpcsToDisplay().forEach((id, npc) -> renderNpcRespawn(npc, graphics));
+		}
+
 		for (NPC npc : plugin.getHighlightedNpcs())
 		{
 			renderNpcOverlay(graphics, npc, npc.getName(), config.getHighlightColor());
@@ -66,22 +93,81 @@ public class NpcClickboxOverlay extends Overlay
 		return null;
 	}
 
+	private void renderNpcRespawn(final MemorizedNpc npc, final Graphics2D graphics)
+	{
+		if (npc.getPossibleRespawnLocations().isEmpty())
+		{
+			return;
+		}
+
+		final WorldPoint respawnLocation = npc.getPossibleRespawnLocations().get(0);
+		final LocalPoint lp = LocalPoint.fromWorld(client, respawnLocation.getX(), respawnLocation.getY());
+
+		if (lp == null)
+		{
+			return;
+		}
+
+		final Color color = config.getHighlightColor();
+
+		final LocalPoint centerLp = new LocalPoint(
+			lp.getX() + Perspective.LOCAL_TILE_SIZE * (npc.getNpcSize() - 1) / 2,
+			lp.getY() + Perspective.LOCAL_TILE_SIZE * (npc.getNpcSize() - 1) / 2);
+
+		final Polygon poly = Perspective.getCanvasTileAreaPoly(client, centerLp, npc.getNpcSize());
+
+		if (poly != null)
+		{
+			OverlayUtil.renderPolygon(graphics, poly, color);
+		}
+
+		final Instant now = Instant.now();
+		final double baseTick = (npc.getDiedOnTick() + npc.getRespawnTime()) - client.getTickCount() * ESTIMATED_TICK_LENGTH;
+		final double sinceLast = (now.toEpochMilli() - plugin.getLastTickUpdate().toEpochMilli()) / 1000.0;
+		final double timeLeft = Math.max(0.0, baseTick - sinceLast);
+		final String timeLeftStr = TIME_LEFT_FORMATTER.format(timeLeft);
+
+		final int textWidth = graphics.getFontMetrics().stringWidth(timeLeftStr);
+		final int textHeight = graphics.getFontMetrics().getAscent();
+
+		final Point canvasPoint = Perspective
+			.worldToCanvas(client, centerLp.getX(), centerLp.getY(), respawnLocation.getPlane());
+
+		if (canvasPoint != null)
+		{
+			final Point canvasCenterPoint = new Point(
+				canvasPoint.getX() - textWidth / 2,
+				canvasPoint.getY() + textHeight / 2);
+
+			OverlayUtil.renderTextLocation(graphics, canvasCenterPoint, timeLeftStr, TEXT_COLOR);
+		}
+	}
+
 	private void renderNpcOverlay(Graphics2D graphics, NPC actor, String name, Color color)
 	{
 		switch (config.renderStyle())
 		{
 			case TILE:
-				Polygon objectTile = actor.getCanvasTilePoly();
+			{
+				int size = 1;
+				NPCComposition composition = actor.getTransformedComposition();
+				if (composition != null)
+				{
+					size = composition.getSize();
+				}
+				LocalPoint lp = actor.getLocalLocation();
+				Polygon tilePoly = Perspective.getCanvasTileAreaPoly(client, lp, size);
 
-				if (objectTile != null)
+				if (tilePoly != null)
 				{
 					graphics.setColor(color);
 					graphics.setStroke(new BasicStroke(2));
-					graphics.draw(objectTile);
+					graphics.draw(tilePoly);
 					graphics.setColor(new Color(color.getRed(), color.getGreen(), color.getBlue(), 20));
-					graphics.fill(objectTile);
+					graphics.fill(tilePoly);
 				}
 				break;
+			}
 
 			case HULL:
 				Polygon objectClickbox = actor.getConvexHull();