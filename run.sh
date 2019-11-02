45f9a53e3d6c2494db28438876ebb7f51e2f0e92
Chris Johnson <cjohnson@covermymeds.com>
2019-11-02 19:46:09 -0400

    Remove --debug from run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 release=$(ls runelite-client/build/libs/*shaded.jar | sort --version-sort | tail -n1)
-java -ea -jar $release --debug --developer-mode
+java -ea -jar $release --developer-mode
